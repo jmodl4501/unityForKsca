ea2baa5bbda5ceae69dbaee213537043ce00f93e
jmodl4501 <x2twoi@naver.com>
2025-03-11 06:46:18 +0900

    next: 1) IntroScene 2) JoyStick Control 3) Learn Animation

---
+++ Assets/mjhCustom/Scripts/GameScene.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class  GameScene : MonoBehaviour
+{
+    [SerializeField] private float gravityValue = -9.81f;
+
+    [SerializeField] UserController userController;
+
+    
+    private void Start() {
+        
+    }
+    private void Update() {
+        
+    }
+    
+    public void jump(){
+
+    }
+
+    public void exit(){
+
+    }    
+}
---
+++ Assets/mjhCustom/Scripts/UserController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class  UserController : MonoBehaviour
+{
+    [SerializeField] GameObject user;
+    private void Start() {
+        
+    }
+    private void Update() {
+        
+    }
+    
+    public void jump(){
+
+    }
+}
